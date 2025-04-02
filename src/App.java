b3656819ae39fb540b127297609c015272c88641
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-04-01 20:05:34 -0500

    Correcciones sugeridas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
+import controllers.ManagePeople;
+//import models.Person;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        //Person person = new Person( "Ivanna", 18);
+        //System.out.println(person.getName());
+        ManagePeople mG = new ManagePeople();
+        mG.sortPeopleByAge();
+        mG.sortPeopleByName();
     }
+
 }
+
